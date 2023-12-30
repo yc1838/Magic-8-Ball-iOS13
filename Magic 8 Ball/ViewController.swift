38b0b14cfff92844c0e47f9e911e957acff409a7
yc1838 <yc1838@nyu.edu>
2023-12-30 01:30:27 -0500

    updated

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,7 +12,11 @@ class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
+    @IBOutlet weak var ballViewOne: UIImageView!
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        ballViewOne.image = ballArray.randomElement()
+    }
+    
 
 }
 
